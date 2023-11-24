7fdb1bc8281439cac4b539cd7c59344e8e032d4c
edgar <edgar.dev22@gmail.com>
2023-11-24 14:44:03 -0300

    Requisito 04

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_BAD_REQUEST_STATUS = 400;
 const PORT = process.env.PORT || 3002;
 const TALKER_FILE_PATH = path.join(__dirname, 'talker.json');
 
@@ -21,6 +22,44 @@ const loadTalkerData = () => {
     console.error('Erro ao carregar dados do arquivo:', err);
     return [];
   }
+};
+
+// Middleware para validar o formato do email
+const validateEmailFormat = (req, res, next) => {
+  const { email } = req.body;
+  if (!email || !email.trim()) {
+    return res.status(HTTP_BAD_REQUEST_STATUS).json({
+      message: 'O campo "email" é obrigatório',
+    });
+  }
+
+  // Utilize uma expressão regular simples para validar o formato do email
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    return res.status(HTTP_BAD_REQUEST_STATUS).json({
+      message: 'O "email" deve ter o formato "email@email.com"',
+    });
+  }
+
+  next();
+};
+
+// Middleware para validar a senha
+const validatePassword = (req, res, next) => {
+  const { password } = req.body;
+  if (!password || !password.trim()) {
+    return res.status(HTTP_BAD_REQUEST_STATUS).json({
+      message: 'O campo "password" é obrigatório',
+    });
+  }
+
+  if (password.length < 6) {
+    return res.status(HTTP_BAD_REQUEST_STATUS).json({
+      message: 'O "password" deve ter pelo menos 6 caracteres',
+    });
+  }
+
+  next();
 };
 
 // Endpoint para obter todas as pessoas palestrantes
@@ -42,12 +81,10 @@ app.get('/talker/:id', (req, res) => {
   }
 });
 
-app.post('/login', (req, res) => {
-  // const { email, password } = req.body;
+app.post('/login', validateEmailFormat, validatePassword, (req, res) => {
   const randomToken = crypto.randomBytes(8).toString('hex');
   res.status(HTTP_OK_STATUS).json({ token: randomToken });
 });
-
 // Não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();

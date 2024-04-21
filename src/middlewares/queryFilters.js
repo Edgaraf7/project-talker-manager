1c33dd54c9ad8deba13b3bf911b9be0639facd8d
edgar <edgar.dev22@gmail.com>
2024-04-21 10:16:20 -0300

    Reorganizando

---
+++ src/middlewares/queryFilters.js
@@ -0,0 +1,53 @@
+const utils = require('../utils/utils');
+const { DATE_REGEX } = require('../utils/consts');
+
+const rateFilter = (req, res, next) => {
+  const { rate } = req.query;
+  const data = utils.readTalkersList();
+
+  if (rate === undefined) {
+    req.body.data = data;
+    next();
+  }
+
+  const rateNumber = Number(rate);
+
+  if (!Number.isInteger(rateNumber) || rateNumber > 5 || rateNumber < 1) {
+    return res.status(400)
+      .json({ message: 'O campo "rate" deve ser um número inteiro entre 1 e 5' });
+  }
+
+  req.body.data = data.filter(({ talk }) => talk.rate === rateNumber);
+  next();
+};
+
+const dateFilter = (req, res, next) => {
+  const { date } = req.query;
+  const { data } = req.body;
+
+  if (date === undefined || date.trim() === '') {
+    next();
+  }
+
+  if (!DATE_REGEX.test(date)) {
+    return res.status(400).json({ message: 'O parâmetro "date" deve ter o formato "dd/mm/aaaa"' });
+  }
+
+  req.body.data = data.filter(({ talk }) => talk.watchedAt === date);
+  next();
+};
+
+const nameFilter = (req, res) => {
+  const { q } = req.query;
+  const { data } = req.body;
+
+  if (!q) return res.status(200).json(data);
+
+  const searchedData = data.filter(({ name }) => name.toLowerCase().includes(q.toLowerCase()));
+
+  return res.status(200).json(searchedData);
+};
+
+const queryFilters = [rateFilter, dateFilter, nameFilter];
+
+module.exports = queryFilters;
---
+++ src/routes/talkerRoutes.js
@@ -0,0 +1,26 @@
+const { Router } = require('express');
+const postValidators = require('../middlewares/postValidators');
+const validateToken = require('../middlewares/auth');
+const validateRate = require('../middlewares/rateValitor');
+const queryFilters = require('../middlewares/queryFilters');
+const talkerController = require('../controllers/talkerController');
+
+const router = Router();
+
+router.get('/', talkerController.getAllTalkers);
+
+router.get('/db', talkerController.getTalkersFromDB);
+
+router.get('/search', validateToken, queryFilters);
+
+router.get('/:id', talkerController.getTalkerById);
+
+router.post('/', validateToken, postValidators, talkerController.addNewTalker);
+
+router.put('/:id', validateToken, postValidators, talkerController.updateTalker);
+
+router.patch('/rate/:id', validateToken, validateRate, talkerController.patchTalkerId);
+
+router.delete('/:id', validateToken, talkerController.deleteTalker);
+
+module.exports = router;
